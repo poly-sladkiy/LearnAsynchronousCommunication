bb84b83c9e8553180ab27331d6d00735df9688f9
Konstantin Ignakov <ignakov37@gmail.com>
2024-10-25 03:01:37 +0300

    Добавлен простой consumer с web api

---
+++ Example MassTransit Pub Sub/MassTransit WebApi Consumer/Consumers/SimpleModelConsumer.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using MassTransit;
+using MassTransit_Pub_Sub_Contracts.Models;
+
+namespace MassTransit_WebApi_Consumer.Consumers;
+
+public class SimpleModelConsumer : IConsumer<SimpleModel>
+{
+
+	public Task Consume(ConsumeContext<SimpleModel> context)
+	{
+		// _logger.LogInformation($"Model consumed: {JsonSerializer.Serialize(context.Message)}");
+		
+		Console.WriteLine($"Model consumed: {JsonSerializer.Serialize(context.Message)}");
+		
+		return Task.CompletedTask;
+	}
+}
